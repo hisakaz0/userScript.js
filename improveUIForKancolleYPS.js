18c839c0c644b229a1f83b72a3e71ec653fbdde3
Hisakazu Fukuoka <cantabilehisa@gmail.com>
2016-02-14 21:49:55 +0900

    Create improveUIForKancolleYPS.js

---
+++ improveUIForKancolleYPS.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         improveUIForKancolleYPS
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  show date on html title area
+// @author       pinkienort
+// @match        http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+// 'use strict';
+
+// Your code here...
